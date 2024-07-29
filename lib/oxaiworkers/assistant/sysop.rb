0937bcf37a0acee41e704169a5a598aa98f18026
Smolev Denis <smolev@me.com>
2024-07-29 22:27:05 +0300

    refactoring

--- lib/oxaiworkers/assistant/sysop.rb
+++ lib/oxaiworkers/assistant/sysop.rb
@@ -5,8 +5,12 @@ module OxAiWorkers
       def initialize delayed: false, model: nil
         worker = delayed ? OxAiWorkers::DelayedRequest.new : OxAiWorkers::Request.new
         worker.model = model || OxAiWorkers.configuration.model
-        @iterator = OxAiWorkers::Iterator.new(worker: worker, tools: [OxAiWorkers::Tool::Eval.new])
-        @iterator.role = I18n.t("oxaiworkers.assistant.sysop.role")
+
+        @iterator = OxAiWorkers::Iterator.new(
+          worker: worker,
+          role: I18n.t("oxaiworkers.assistant.sysop.role"),
+          tools: [OxAiWorkers::Tool::Eval.new]
+          )
       end
 
       def setTask task
