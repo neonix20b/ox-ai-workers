d0888e3ebc3888a9ef7c36d73c504571dd8d2ec7
Smolev Denis <smolev@me.com>
2024-07-30 15:45:29 +0300

    new tools: file system, database

---
+++ lib/oxaiworkers/tool/database.rb
@@ -0,0 +1,119 @@
+module OxAiWorkers
+  module Tool
+    #
+    # Connects to a database, executes SQL queries, and outputs DB schema for Agents to use
+    #
+    # Gem requirements:
+    #     gem "sequel", "~> 5.68.0"
+    #
+    # Usage:
+    #     database = OxAiWorkers::Tool::Database.new(connection_string: "postgres://user:password@localhost:5432/db_name")
+    #
+    class Database
+      extend OxAiWorkers::ToolDefinition
+      include OxAiWorkers::DependencyHelper
+
+      define_function :list_tables,
+                      description: I18n.t('oxaiworkers.tool.database_tool.list_tables.description')
+
+      define_function :describe_tables,
+                      description: I18n.t('oxaiworkers.tool.database_tool.describe_tables.description') do
+        property :tables, type: 'string',
+                          description: I18n.t('oxaiworkers.tool.database_tool.describe_tables.tables'),
+                          required: true
+      end
+
+      define_function :dump_schema,
+                      description: I18n.t('oxaiworkers.tool.database_tool.dump_schema.description')
+
+      define_function :execute,
+                      description: I18n.t('oxaiworkers.tool.database_tool.execute.description') do
+        property :input, type: 'string',
+                         description: I18n.t('oxaiworkers.tool.database_tool.execute.input'),
+                         required: true
+      end
+
+      attr_reader :db, :requested_tables, :excluded_tables
+
+      # Establish a database connection
+      #
+      # @param connection_string [String] Database connection info, e.g. 'postgres://user:password@localhost:5432/db_name'
+      # @param tables [Array<Symbol>] The tables to use. Will use all if empty.
+      # @param except_tables [Array<Symbol>] The tables to exclude. Will exclude none if empty.
+      # @return [Database] Database object
+      def initialize(connection_string:, tables: [], exclude_tables: [])
+        depends_on 'sequel'
+
+        raise StandardError, 'connection_string parameter cannot be blank' if connection_string.empty?
+
+        @db = Sequel.connect(connection_string)
+        @requested_tables = tables
+        @excluded_tables = exclude_tables
+      end
+
+      # Database Tool: Returns a list of tables in the database
+      def list_tables
+        db.tables
+      end
+
+      # Database Tool: Returns the schema for a list of tables
+      #
+      # @param tables [String] The tables to describe.
+      # @return [String] Database schema for the tables
+      def describe_tables(tables:)
+        schema = ''
+        tables.split(',').each do |table|
+          describe_table(table, schema)
+        end
+        schema
+      end
+
+      # Database Tool: Returns the database schema
+      #
+      # @return [String] Database schema
+      def dump_schema
+        puts('Dumping schema tables and keys', for: self.class)
+        schema = ''
+        db.tables.each do |table|
+          describe_table(table, schema)
+        end
+        schema
+      end
+
+      def describe_table(table, schema)
+        primary_key_columns = []
+        primary_key_column_count = db.schema(table).count { |column| column[1][:primary_key] == true }
+
+        schema << "CREATE TABLE #{table}(\n"
+        db.schema(table).each do |column|
+          schema << "#{column[0]} #{column[1][:type]}"
+          if column[1][:primary_key] == true
+            schema << ' PRIMARY KEY' if primary_key_column_count == 1
+          else
+            primary_key_columns << column[0]
+          end
+          schema << ",\n" unless column == db.schema(table).last && primary_key_column_count == 1
+        end
+        schema << "PRIMARY KEY (#{primary_key_columns.join(',')})" if primary_key_column_count > 1
+        db.foreign_key_list(table).each do |fk|
+          schema << ",\n" if fk == db.foreign_key_list(table).first
+          schema << "FOREIGN KEY (#{fk[:columns][0]}) REFERENCES #{fk[:table]}(#{fk[:key][0]})"
+          schema << ",\n" unless fk == db.foreign_key_list(table).last
+        end
+        schema << ");\n"
+      end
+
+      # Database Tool: Executes a SQL query and returns the results
+      #
+      # @param input [String] SQL query to be executed
+      # @return [Array] Results from the SQL query
+      def execute(input:)
+        puts("Executing \"#{input}\"", for: self.class)
+
+        db[input].to_a
+      rescue Sequel::DatabaseError => e
+        e.message
+      end
+    end
+  end
+end
---
+++ lib/oxaiworkers/tool/file_system.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module OxAiWorkers
+  module Tool
+    #
+    # A tool that wraps the Ruby file system classes.
+    #
+    # Usage:
+    #    file_system = OxAiWorkers::Tool::FileSystem.new
+    #
+    class FileSystem
+      extend OxAiWorkers::ToolDefinition
+
+      define_function :list_directory,
+                      description: I18n.t('oxaiworkers.tool.file_system.list_directory.description') do
+        property :directory_path, type: 'string',
+                                  description: I18n.t('oxaiworkers.tool.file_system.list_directory.directory_path'),
+                                  required: true
+      end
+
+      define_function :read_file, description: I18n.t('oxaiworkers.tool.file_system.read_file.description') do
+        property :file_path, type: 'string', description: I18n.t('oxaiworkers.tool.file_system.read_file.file_path'),
+                             required: true
+      end
+
+      define_function :write_to_file, description: I18n.t('oxaiworkers.tool.file_system.write_to_file.description') do
+        property :file_path, type: 'string',
+                             description: I18n.t('oxaiworkers.tool.file_system.write_to_file.file_path'), required: true
+        property :content, type: 'string', description: I18n.t('oxaiworkers.tool.file_system.write_to_file.content'),
+                           required: true
+      end
+
+      def list_directory(directory_path:)
+        Dir.entries(directory_path)
+      rescue Errno::ENOENT
+        "No such directory: #{directory_path}"
+      end
+
+      def read_file(file_path:)
+        File.read(file_path)
+      rescue Errno::ENOENT
+        "No such file: #{file_path}"
+      end
+
+      def write_to_file(file_path:, content:)
+        File.write(file_path, content)
+      rescue Errno::EACCES
+        "Permission denied: #{file_path}"
+      end
+    end
+  end
+end
